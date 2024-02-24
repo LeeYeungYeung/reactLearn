a1c8af972ffa767e458c40a8631853f02eaf18bd
_leeyeung <73725183+LeeYeungYeung@users.noreply.github.com>
2024-02-24 22:37:07 +0800

    updaet

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -13,11 +13,7 @@ import clsx from 'clsx';
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/ui/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/ui/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
+  { name: 'Invoices', href: '/ui/dashboard/invoices', icon: DocumentDuplicateIcon },
   { name: 'Customers', href: '/ui/dashboard/customers', icon: UserGroupIcon },
 ];
 
