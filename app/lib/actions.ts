314584298f1c062a62514d64ffd27d7eea51994f
LeeYeungYeung <2487523364@qq.com>
2024-03-01 19:54:58 +0800

    update

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -34,20 +34,35 @@ export async function updateInvoice(
      
       const amountInCents = amount * 100;
      
-      await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-      `;
+      try {
+          await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+          `;
+        
+      } catch (error) {
+        return {
+            message: 'Database Error: Failed to update',
+        }
+      }
      
       revalidatePath('/dashboard/invoices');
       redirect('/dashboard/invoices');
 }
 
 export async function  deleteInvoice(id : string) { 
-
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
+    throw new Error("Failed to Delete Invoice");
+    
+    try{
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+    }catch(error){
+        return {
+            message: 'Database Error: Failed to Delete',
+        }
+    }
+    
 }
 
 export async function createInvoive(formData: FormData){
@@ -59,11 +74,17 @@ export async function createInvoive(formData: FormData){
     console.log(typeof amount);
    const amountInCents = amount * 100;
    const date = new Date().toISOString().split('T')[0];
+   try{
 
-   await sql`
-   INSERT INTO invoices (customer_id, amount, status, date)
-   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-   `
+       await sql`
+       INSERT INTO invoices (customer_id, amount, status, date)
+       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+       `
+   }catch(error){
+    return {
+        message: 'Database Error: Failed to Create Invoive',
+    }
+   }
     revalidatePath('/ui/dashboard/invoices');
     redirect('/ui/dashboard/invoices');
 }
